dbd25f6cc4ceba26967a0278da3b6e7799f2199a
Jong Bum Kim <Jonbkim7@gmail.com>
2024-06-16 16:18:36 -0400

    refactor: changed .env system to global variable within apiAuth + Added more descriptive photonames

--- apiAuth.py
+++ apiAuth.py
@@ -1,13 +1,7 @@
 from openai import OpenAI
-from dotenv import load_dotenv
-import os
+
+my_api_key = "enter your api key here"
 
 def authGPT() -> OpenAI:
-    # Load environment variables from .env file
-    load_dotenv()
-
-    # Access the API key from environment variables
-    my_api_key = os.getenv("OPENAI_API_KEY")
-
     client = OpenAI(api_key = my_api_key )
     return client
