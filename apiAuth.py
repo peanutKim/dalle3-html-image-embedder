90fdf8623238dd31bbf132984e822f37ca7f3bd3
Jong Bum Kim <Jonbkim7@gmail.com>
2024-06-21 22:30:29 -0400

    refactor: making GPT authentification changes

--- apiAuth.py
+++ apiAuth.py
@@ -1,13 +1,9 @@
 from openai import OpenAI
-from dotenv import load_dotenv
 import os
 
 def authGPT() -> OpenAI:
-    # Load environment variables from .env file
-    load_dotenv()
-
-    # Access the API key from environment variables
+    # Access the API key directly from environment variables
     my_api_key = os.getenv("OPENAI_API_KEY")
 
-    client = OpenAI(api_key = my_api_key )
+    client = OpenAI(api_key=my_api_key)
     return client
