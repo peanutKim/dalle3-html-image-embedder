7d637799491173cfcbad867e9dfb67fe0cfec395
Jong Bum Kim <Jonbkim7@gmail.com>
2024-06-22 00:30:40 -0400

    refactor: removing all the git config lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,7 @@
 set -e # Increase bash strictness
 set -o pipefail
 
-# Configure Git user identity
-git config --global user.email "action@github.com"
-git config --global user.name "GitHub Action"
-
 git config --global --add safe.directory /github/workspace
-chmod -R u+rwX,g+rwX,o+rwX .git
-
-# Debugging permissions
-ls -la .git
-whoami
 
 python /main.py
 ls -la /github/workspace
