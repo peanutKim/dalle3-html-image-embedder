d37d42c976655494aff2d020861d6704aa5bf8b6
Jong Bum Kim <Jonbkim7@gmail.com>
2024-06-22 00:18:41 -0400

    feat: adding git configs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 set -e # Increase bash strictness
 set -o pipefail
 
+# Configure Git user identity
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+
 git config --global --add safe.directory /github/workspace
-
 python /main.py
-
 ls -la /github/workspace
